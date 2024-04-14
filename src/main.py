7ddae7f827baa299f98b511c234f78e3fca5e6b7
aleshkin907 <116716524+aleshkin907@users.noreply.github.com>
2024-04-14 14:06:54 +0600

    feat(parser): create a lounch loop & add logging (#8)
    * delete alembic commits
    
    * add a launch loop

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import time
+import schedule
 
 from db.db import session
 from repositories.redis_repository import RedisRepository
@@ -11,7 +12,17 @@ from services.server_service import ServerService
 from logging_loguru.loguru import get_logger
 
 
-logger =get_logger()
+logger = get_logger()
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        logger.info(f"Execution time of {func.__name__}: {end_time - start_time} seconds")
+        return result
+    return wrapper
 
 
 if __name__ == "__main__":
@@ -26,10 +37,22 @@ if __name__ == "__main__":
     parser = Parser(settings.SECRET_KEY, settings.HASHRATE_API_KEY, gpu_service, server_service)
 
 
-    @logger.catch
-    def parse():
+    @measure_time
+    def parse_wrapper():
         parser.parse()
 
 
-    parse()
-    parser.update_servers_profit()
+    @measure_time
+    def update_servers_profit_wrapper():
+        parser.update_servers_profit()
+
+
+    parse_wrapper()
+    update_servers_profit_wrapper()
+    
+    schedule.every(5).minutes.do(parse_wrapper)
+    schedule.every(5).minutes.do(update_servers_profit_wrapper)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
